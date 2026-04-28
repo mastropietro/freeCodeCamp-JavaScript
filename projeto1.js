03180765eb6f1b678397e6b9fbaf8de214615162
mastropietro <nichollas.mastropietro.rs@gmail.com>
2026-04-28 14:46:16 -0300

    Adicionei os meus 4 primeiros projetos JavaScript.

---
+++ projeto1.js
@@ -0,0 +1,15 @@
+// Este é meu primeiro programa em Java! Fiz um robôzinho meu :)
+
+console.log("Hello! I'm your coding fun fact guide!");
+const botName = "BotDo00";
+const botLocation = "São Paulo";
+const favoriteLanguage = "Java";
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+let codingFact = "A curiosity of " + favoriteLanguage + " is, it was created in only 10 days!";
+console.log(codingFact);
+codingFact = "Another fact, it is he original name of " + favoriteLanguage + " was Oak";
+console.log(codingFact);
+codingFact = "The last curiosity is: in 2010, Oracle Corporation completed its acquisition of Sun Microsystems, taking control of the development of the " + favoriteLanguage + " programming language.";
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");

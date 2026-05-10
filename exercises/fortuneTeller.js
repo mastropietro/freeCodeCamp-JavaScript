29e32c961a2b4dbb0853a914d0877e3e92a1181f
mastropietro <nichollas.mastropietro.rs@gmail.com>
2026-05-10 19:48:11 -0300

    Exercises of the Booleans and Numbers module.

---
+++ exercises/fortuneTeller.js
@@ -0,0 +1,30 @@
+// 9th JavaScript Project. Fortune Teller.
+
+const fortune1 = "Your cat will look very cuddly today.";
+const fortune2 = "The weather will be nice tomorrow.";
+const fortune3 = "Be cautious of your new neighbors.";
+const fortune4 = "You will find a new hobby soon.";
+const fortune5 = "It would be wise to avoid the color red today.";
+
+// Generate a random number between 1 and 5, inclusive.
+// The correct approach would be to use Math.floor instead of Math.round (which I used initially), as otherwise it would go from 1 to 6.
+let randomNumber = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+let selectedFortune;
+
+// Utilize "===" for value comparison.
+if (randomNumber === 1) { 
+  selectedFortune = fortune1
+}
+else if (randomNumber === 2) {
+  selectedFortune = fortune2
+}
+else if (randomNumber === 3) {
+  selectedFortune = fortune3
+}
+else if (randomNumber === 4) {
+  selectedFortune = fortune4
+}
+else if (randomNumber === 5) { 
+  selectedFortune = fortune5
+}
+console.log(selectedFortune)
---
+++ exercises/logicCheck.js
@@ -0,0 +1,22 @@
+// 7th JavaScript Project. Logic Checker Application.
+
+const hasDeveloperJob = true;
+
+// if statement checks if the condition is true and executes the code block if it is.
+if (hasDeveloperJob) {
+  console.log("Timmy is employed as a developer.");
+}
+
+const isTimmyAGamer = false;
+
+if (isTimmyAGamer) {
+  console.log("Timmy loves to play World of Warcraft.");
+}
+
+const timmyAge = 18;
+
+if (timmyAge >= 16) {
+  console.log("Timmy is old enough to drive.");
+} else { // else statement executes a block of code if the condition in the if statement is false.
+  console.log("Timmy is not old enough to drive.");
+}
